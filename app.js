76443f8854856d3f261c5380820f9c7f151bc5a4
Mertkan Alacahan <mertkanalacahan@gmail.com>
2021-08-23 19:36:02 +0300

    Create campground model
    mongoDB connection established and initial campground model has been set

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://localhost:27017/campbook", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Database connected.");
+});
 const app = express();
 
 app.set("view engine", "ejs");
@@ -7,8 +21,14 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get('/', (req, res) => {
     res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({ title: "My backyard", description: "cheap camping" });
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
     console.log("Serving on port 3000");
-})
+});
